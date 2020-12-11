a8fbb62c7ef8b298e8ef899b4fd34b9470dd04ea
Kropotov-Vadim <dooondron@gmail.com>
2020-12-11 15:04:31 +0300

    Swiper

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,49 @@
-$('.main__slider').slick({
-  centerMode: true,
-  slidesToShow: 5,
-  speed: 150,
-  dote: false,
-  variableWidth: true,
-  draggable: false,
-  prevArrow: $('.main__slider-prev'),
-  nextArrow: $('.main__slider-next')
-});
+// $('.main__slider').slick({
+//   centerMode: true,
+//   slidesToShow: 5,
+//   speed: 150,
+//   dote: false,
+//   variableWidth: true,
+//   draggable: false,
+//   prevArrow: $('.main__slider-prev'),
+//   nextArrow: $('.main__slider-next')
+// });
+
+let cSlide = false
+
+let mSlider = new Swiper('.main__wrap', {
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  loop: true,
+  roundLengths: true,
+  centeredSlidesBounds: true,
+  navigation: {
+    nextEl: '.main__slider-next',
+    prevEl: '.main__slider-prev',
+  },
+  on: {
+    init: function(swiper) {
+      swiper.slides[swiper.activeIndex].classList.add('active')
+      if(!cSlide) {
+        cSlide = swiper.slides[swiper.activeIndex]
+      }
+      swiper.resize.resizeHandler()
+    }
+  }
+})
+
+
+mSlider.on('slideChange', ()=> {
+  mSlider.resize.resizeHandler()
+  mSlider.slides[mSlider.activeIndex].classList.add('active')
+  if(!cSlide) {
+    cSlide = mSlider.slides[mSlider.activeIndex]
+  } else {
+    cSlide.classList.remove('active')
+    cSlide = mSlider.slides[mSlider.activeIndex]
+  }
+  
+})
 
 $('.course__select').on('click', () => {
   $('.course__select').toggleClass('active')
